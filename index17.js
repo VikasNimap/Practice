366e436c0cde826134faf8f5d002659755a503d3
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2022-11-24 17:10:46 +0530

    Create index17.js

---
+++ index17.js
@@ -0,0 +1,85 @@
+// let data = [1,2,3,45,66];
+// data[3] = undefined
+// console.log(data);
+
+// let data = [1,2,3,45,66];
+// let position = 3;
+// for(let i = position;i<data.length;i++){
+//     // console.log(data[i]);
+//     data[i]=data[i+1]
+// }
+// data.length = data.length-1
+// console.log(data);
+
+
+// let data = [1,22,77,65,90,333];
+// let max = data[0];
+// for(let i =0;i<data.length;i++){
+//     if(data[i]>max){
+//         max=data[i]
+//     }
+// }
+// console.log(max);
+// let num1 = 20;
+// let num2 = 50;
+// num1=num2-num1
+// num2=num2-num1
+// num1=num1+num2
+// console.log(num1);
+// console.log(num2);
+
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = num1 //1
+// num1=num2 //1
+// num2=num3
+
+// console.log(num1);
+// console.log(num2);
+// let data = [1,22,33,94,77,97,96,44,56,666,888];        2nd max by Satya Bhai
+// let max1  = 0
+// let max2 = 0
+// for(let i = 0;i<data.length;i++){
+//     if(data[i]>max1){
+//         max2=max1
+//         max1 = data[i]
+//     }
+//     else if(data[i]>max2) {
+//         max2=data[i]
+//     }
+// }
+// console.log(max2);
+
+// //i=0=>1
+// //max1=0
+// //max2=0
+
+
+// //i=1=>
+
+// let data = [1,22,35,55,11,73,56];
+// let max1 = 0;
+// let max2 = 0;
+// for(let i = 0;i<data.length;i++){
+//     if(data[i]>max1){
+//         max1 = max2
+//         max2 = data[i]
+//     }
+// }
+// console.log(max1);
+//i=1 => 22
+//max1 = 0
+//max2 = 1
+// let array = [99, 88, 77, 44, 12,100,1008];
+// let max1 = 0;
+// let max2 = 0;
+// for(let i = 0;i<array.length;i++){
+//     if(array[i]>max1){
+//         max2=max1
+//         max1 = array[i]
+//     }
+//     else if(array[i]>max2){
+//         max2=array[i]
+//     }
+// }
+// console.log(max2);
