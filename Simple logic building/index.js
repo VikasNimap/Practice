a2f5548363658c340a165a182a4f1ee1f461d11c
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2022-12-05 12:55:06 +0530

    Create index.js

---
+++ Simple logic building/index.js
@@ -0,0 +1,21 @@
+//Q1 // let arr = [454,22,33,44,77,23,11,0,345,354,354,456];         //find second max
+// let max1 = 0;
+// let max2 = 0;
+// for(let i =0;i<arr.length;i++){
+//         if(arr[i]>max1){
+//                 max1=arr[i]
+//         }
+// }
+// for(let i in arr){
+//         if(arr[i]>max2 && arr[i]<max1){
+//                 max2 = arr[i]
+//         }
+// }
+// console.log(max2);
+
+//Q2 // 
+
+
+
+
+
