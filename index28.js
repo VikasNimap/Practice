e99a3cac22298bd9a70071e22914343fa5f38ef4
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2022-12-08 12:30:51 +0530

    Create index28.js

---
+++ index28.js
@@ -0,0 +1,120 @@
+// console.log("v1k4s");
+
+// const express = require('express');
+// const data = require('./data');
+// const port = 3000;
+// const os = require('os');
+// const path = require('path');
+// const dirPath = path.join(__dirname, 'files')
+// const app = express();
+// const fs = require('fs');
+// const http = require('http');
+// const { dir } = require('console');
+// app.get('/',(req,res)=>{
+//         res.send(fs.readFileSync('data.js','utf-8'))
+// })
+// app.get('/about',(req,res)=>{
+//         res.send('<h1>Hello and welcome to node we develop</h1>')
+// })
+// app.get('*',(req,res)=>{
+//         res.send('<h1>404 Page not found.</h1>')
+// })
+// // http.createServer((req,res)=>{
+// //        .listen(3000);
+// app.listen(3000)
+
+// fs.readFileSync('data.js')
+// fs.writeFileSync('a.txt',"Hllo and wlcm to node")
+// fs.unlinkSync('a.txt')
+// const a  = fs.renameSync('utf-8','vk.txt')
+// console.log(a);
+// const a = fs.writeFileSync(dirPath,'vikas.txt','H33lo v1k4a');
+// fs.mkdirSync('files')
+// fs.writeFileSync('files/p.txt','Hello from vikas');
+// let a = fs.readFileSync('files/p.txt','utf-8');
+// console.log(a);
+// fs.appendFileSync('files/p.txt','Hello fro m')
+// fs.unlinkSync('')
+// fs.writeFile('files/p.txt',"Hello from vikas",(data)=>{
+//         console.log("file created.");
+// })
+// const os = require('os');
+// let a = os.cpus();
+// console.log(a);
+
+// console.log(10);
+// const a = 10;
+//  a = 30
+// console.log(a);
+// let a = 12;
+// let p1 = new Promise((resolve, reject)=>{
+//         resolve(10);
+// }).then((data)=>{
+//         console.log(data);
+//         return data * 2
+// }).then((data)=>{
+//         console.log(data);
+//         return data * 2
+// }).then((data)=>{
+//         console.log("the data is: ",data);
+// }).catch((err)=>{
+//         console.log(err);
+// })
+// async function a(){
+//         let p1 = new Promise((resolve, reject)=>{
+//                 setTimeout(() => {
+//                        resolve("Done!!") 
+//                 }, 2000);
+//         })
+//         let res = await p1;
+//         console.log(res);
+
+// }
+// a()
+// let num = fs.readFileSync('a.txt','utf-8');
+// function a(file,c){
+//         if(num==true){
+//                 new Error("bad request")
+//         }
+//         c()
+// }
+// function c(err, data){
+//         if(err){
+//                 console.log(err);
+//         }
+//         else{
+//                 console.log();
+//         }
+// }
+// c()
+
+// console.log(module);
+// const cluster = require("./cluster");
+// cluster.a()
+// const a = path.parse(__filename)
+// console.log(a);
+
+// let a = os.freemem()
+// console.log(a);
+// let b = os.totalmem()
+// console.log(b);
+// let c = os.tmpdir()
+// console.log(c);
+// let a = fs.readFile('a.txt','utf-8',(err,data)=>{
+//         if(err)      {
+//                 console.log("ERORR",err);
+//         }
+//         else{
+//                 console.log(data);
+//         }
+// })
+
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+// emitter.on('messageLogged',function(arg){
+//         console.log("listener is listening.",arg);
+// })
+// emitter.emit('messageLogged',{id: 1,url: "http://"})
+
+
+
